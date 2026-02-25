dd3bbdc4d19590a0288ca26f999d03a1e3146e8b
eardyvvv <eardysprod@gmail.com>
2026-02-25 16:22:59 +0100

    Added BBoxes Switch and updated default prompt

--- __init__.py
+++ __init__.py
@@ -12,22 +12,47 @@ class API_Input_Panel:
                 "positive_prompt": ("STRING", {"default": "", "multiline": True}),
                 "negative_prompt": ("STRING", {"default": "", "multiline": True}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 999999999999999, "step": 1}),
+                "face_only": ("BOOLEAN", {"default": False}),
             },
         }
 
-    RETURN_TYPES = ("STRING", "STRING", "INT", "INT", "INT", "FLOAT", "STRING", "STRING", "INT")
-    RETURN_NAMES = ("img_url", "video_url", "total_frames", "skip_first_frames", "mask_width", "frame_rate", "positive_prompt", "negative_prompt", "seed")
+    RETURN_TYPES = ("STRING", "STRING", "INT", "INT", "INT", "FLOAT", "STRING", "STRING", "INT", "BOOLEAN")
+    RETURN_NAMES = ("img_url", "video_url", "total_frames", "skip_first_frames", "mask_width", "frame_rate", "positive_prompt", "negative_prompt", "seed", "face_only")
     FUNCTION = "get_values"
     CATEGORY = "API"
 
-    def get_values(self, img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed):
-        return (img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed)
+    def get_values(self, img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed, face_only):
+        return (img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed, face_only)
+
+
+class API_BBox_Switch:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "bboxes_body": ("BBOX",),
+                "bboxes_face": ("BBOX",),
+                "face_only": ("BOOLEAN", {"default": False}),
+            }
+        }
+
+    RETURN_TYPES = ("BBOX",)
+    RETURN_NAMES = ("bboxes",)
+    FUNCTION = "switch"
+    CATEGORY = "API"
+
+    def switch(self, bboxes_body, bboxes_face, face_only):
+        if face_only:
+            return (bboxes_face,)
+        return (bboxes_body,)
 
 
 NODE_CLASS_MAPPINGS = {
-    "API_Input_Panel": API_Input_Panel
+    "API_Input_Panel": API_Input_Panel,
+    "API_BBox_Switch": API_BBox_Switch
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "API_Input_Panel": "🛠️ API Input Panel"
+    "API_Input_Panel": "Input Panel",
+    "API_BBox_Switch": "BBoxes Switch"
 }
