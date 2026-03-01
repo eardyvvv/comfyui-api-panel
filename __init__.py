5575966ab29ecbacba5cf62d09106da8463385ec
eardyvvv <eardysprod@gmail.com>
2026-03-01 18:16:14 +0100

    Add width and height inputs

--- __init__.py
+++ __init__.py
@@ -13,16 +13,18 @@ class API_Input_Panel:
                 "negative_prompt": ("STRING", {"default": "", "multiline": True}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 999999999999999, "step": 1}),
                 "face_only": ("BOOLEAN", {"default": False}),
+                "width": ("INT", {"default": 480, "min": 1, "max": 8192, "step": 1}),
+                "height": ("INT", {"default": 856, "min": 1, "max": 8192, "step": 1}),
             },
         }
 
-    RETURN_TYPES = ("STRING", "STRING", "INT", "INT", "INT", "FLOAT", "STRING", "STRING", "INT", "BOOLEAN")
-    RETURN_NAMES = ("img_url", "video_url", "total_frames", "skip_first_frames", "mask_width", "frame_rate", "positive_prompt", "negative_prompt", "seed", "face_only")
+    RETURN_TYPES = ("STRING", "STRING", "INT", "INT", "INT", "FLOAT", "STRING", "STRING", "INT", "BOOLEAN", "INT", "INT")
+    RETURN_NAMES = ("img_url", "video_url", "total_frames", "skip_first_frames", "mask_width", "frame_rate", "positive_prompt", "negative_prompt", "seed", "face_only", "width", "height")
     FUNCTION = "get_values"
     CATEGORY = "API"
 
-    def get_values(self, img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed, face_only):
-        return (img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed, face_only)
+    def get_values(self, img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed, face_only, width, height):
+        return (img_url, video_url, total_frames, skip_first_frames, mask_width, frame_rate, positive_prompt, negative_prompt, seed, face_only, width, height)
 
 
 class API_BBox_Switch:
